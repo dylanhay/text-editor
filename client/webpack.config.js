3dd9a06782f4597f941fe1ac12246e944411ce38
dchay <dylanhay@rogers.com>
2022-10-01 14:57:27 -0700

    workbox added to generate SW

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,7 +1,8 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
+// const { InjectManifest } = require('workbox-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
@@ -21,7 +22,8 @@ module.exports = () => {
       new HtmlWebpackPlugin({
         template: './index.html',
         title: 'Webpack Plugin',
-      })
+      }),
+      new WorkboxPlugin.GenerateSW()
     ],
     module: {
       rules: [
