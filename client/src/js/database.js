03e433bff3696a9b419ece0f7e8b336c7739e714
dchay <dylanhay@rogers.com>
2022-10-01 12:57:32 -0700

    update functionality implemented_rev

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,33 +1,47 @@
-import { openDB } from 'idb';
-import 'regenerator-runtime/runtime';
+import { openDB } from "idb";
 
 export const initdb = async () =>
-  openDB('jate', 1, {
+  openDB("jate", 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+      if (db.objectStoreNames.contains("jate")) {
+        console.log("jate database already exists");
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      db.createObjectStore("jate", { keyPath: "id", autoIncrement: true });
+      console.log("jate database created");
     },
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log("PUT to the database");
 
+  // Create a connection to the database and specify the version we want to use.
+  const contactDb = await openDB("jate", 1);
 
+  // Create a new transaction and specify the store and data privileges.
+  const tx = contactDb.transaction("jate", "readwrite");
 
+  // Open up the desired object store.
+  const store = tx.objectStore("jate");
 
+  // Use the .add() method on the store and pass in the content.
+  const request = store.put({
+    id: 1,
+    content: content,
+  });
+
+  // Get confirmation of the request.
+  const result = await request;
+  console.log("🚀 - data saved to the database", result);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-// export const getDb = async () => console.error('getDb not implemented');
-
 // Export a function we will use to GET to the database.
 export const getDb = async () => {
   console.log('GET from the database');
 
-  // Create a connection to the IndexedDB database and the version we want to use.
+  // Create a connection to the IndexedDB database 
   const contactDb = await openDB('jate', 1);
 
   // Create a new transaction and specify the store and data privileges.
@@ -42,7 +56,7 @@ export const getDb = async () => {
   // Get confirmation of the request.
   const result = await request;
   console.log('result.value', result);
-  return result;
+  return result.value;
 };
 
 initdb();
--- client/src/js/index.js
+++ client/src/js/index.js
@@ -2,8 +2,8 @@ import { Workbox } from "workbox-window";
 import Editor from "./editor";
 import "./database";
 import "../css/style.css";
+
 // import Logo from "../images/logo.png";
-// import { initdb, getDb } from "./database";
 
 const main = document.querySelector("#main");
 main.innerHTML = "";
